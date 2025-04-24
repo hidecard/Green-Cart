18e814b3a1d2c6d2356a923be3590cebca85c3f8
hidecard <hidecard1500@gmail.com>
2025-04-24 11:50:04 +0630

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
+
+const Navbar = () => {
+    const [open, setOpen] = React.useState(false);
+    const { user, setUser, setShowUserLogin } = useAppContext();
+    const navigate = useNavigate();
+
+    const Logout = async () => {
+        setUser(null);
+        navigate('/');
+    };
+
+    return (
+        <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all z-50">
+
+            <NavLink to='/'>
+                <img className="h-9" src={assets.logo} alt="dummyLogoColored" />
+            </NavLink>
+
+            {/* Desktop Menu */}
+            <div className="hidden sm:flex items-center gap-8">
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/products'>All Products</NavLink>
+                <NavLink to='/contact'>Contact</NavLink>
+
+                <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
+                    <input
+                        className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
+                        type="text"
+                        placeholder="Search products"
+                    />
+                    <img src={assets.search_icon} alt="Search" className="w-4 h-4" />
+                </div>
+
+                <div className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt="Cart" className="w-6 opacity-80" />
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                </div>
+
+                {!user ? (
+                    <button onClick={() => setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                        Login
+                    </button>
+                ) : (
+                    <div>
+                        <img src={assets.profile_icon} className="w-10" alt=""/>
+                        <ul>
+                            <li>My Order</li>
+                            <li>Logout</li>
+                        </ul>
+                    </div>
+                )}
+            </div>
+
+            {/* Mobile Menu Button */}
+            <button onClick={() => setOpen(!open)} aria-label="Menu" className="sm:hidden">
+                <img src={assets.menu_icon} alt="Menu" />
+            </button>
+
+            {/* Mobile Menu List */}
+            {open && (
+                <div className="absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex flex-col items-start gap-3 px-5 text-sm sm:hidden z-10">
+                    <NavLink to='/' onClick={() => setOpen(false)} className="w-full">Home</NavLink>
+                    <NavLink to="/products" onClick={() => setOpen(false)} className="w-full">All Products</NavLink>
+                    {user && (
+                        <NavLink to="/orders" onClick={() => setOpen(false)} className="w-full">My Orders</NavLink>
+                    )}
+                    <NavLink to='/contact' onClick={() => setOpen(false)} className="w-full">Contact</NavLink>
+
+                    {!user ? (
+                        <button
+                            onClick={() => {
+                                setOpen(false);
+                                setShowUserLogin(true);
+                            }}
+                            className="w-full mt-2 px-6 py-2 bg-primary text-white rounded-full text-sm"
+                        >
+                            Login
+                        </button>
+                    ) : (
+                        <button
+                            onClick={() => {
+                                setOpen(false);
+                                Logout();
+                            }}
+                            className="w-full mt-2 px-6 py-2 bg-primary text-white rounded-full text-sm"
+                        >
+                            Logout
+                        </button>
+                    )}
+                </div>
+            )}
+        </nav>
+    );
+};
+
+export default Navbar;
